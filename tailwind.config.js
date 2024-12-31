12608a48a9363c396500cc049ae784ca248dd455
Eduardo Maciel <eduardomacielbr@gmail.com>
2024-12-31 10:40:37 -0300

    🔧 raw: implemented tailwind and material packages

---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: [
+		"./src/**/*.{html,ts}", // Inclua os arquivos do projeto
+		"./node_modules/@angular/material/**/*.js", // Adicione o Angular Material
+	],
+	theme: {
+		extend: {
+			colors: {
+				// Usando variáveis CSS para temas dinâmicos
+				primary: "var(--mat-sys-primary)",
+				"on-primary": "var(--mat-sys-on-primary)",
+				"primary-container": "var(--mat-sys-primary-container)",
+				"on-primary-container": "var(--mat-sys-on-primary-container)",
+				secondary: "var(--mat-sys-secondary)",
+				"on-secondary": "var(--mat-sys-on-secondary)",
+				"secondary-container": "var(--mat-sys-secondary-container)",
+				"on-secondary-container": "var(--mat-sys-on-secondary-container)",
+				tertiary: "var(--mat-sys-tertiary)",
+				"on-tertiary": "var(--mat-sys-on-tertiary)",
+				"tertiary-container": "var(--mat-sys-tertiary-container)",
+				"on-tertiary-container": "var(--mat-sys-on-tertiary-container)",
+				background: "var(--mat-sys-background)",
+				"on-background": "var(--mat-sys-on-background)",
+				surface: "var(--mat-sys-surface)",
+				"on-surface": "var(--mat-sys-on-surface)",
+				outline: "var(--mat-sys-outline)",
+				error: "var(--mat-sys-error)",
+				"error-container": "var(--mat-sys-error-container)",
+				"on-error": "var(--mat-sys-on-error)",
+				"on-error-container": "var(--mat-sys-on-error-container)",
+				"surface-variant": "var(--mat-sys-surface-variant)",
+				"on-surface-variant": "var(--mat-sys-on-surface-variant)",
+			},
+		},
+	},
+	plugins: [],
+	darkMode: "class", // Ativa o modo escuro baseado em uma classe
+};
