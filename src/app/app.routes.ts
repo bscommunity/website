3b923ec60de21b0135a985540a5411ffa58bde25
Eduardo Maciel <eduardomacielbr@gmail.com>
2024-12-31 10:58:56 -0300

    ✨ feat: initial routing pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import type { Routes } from "@angular/router";
 
-export const routes: Routes = [];
+// Routes
+import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
+import { PublishedComponent } from "./published/published.component";
+
+export const routes: Routes = [
+	{ path: "published", component: PublishedComponent, title: "Published" },
+	{ path: "", redirectTo: "/published", pathMatch: "full" },
+	// The 404 page should be the last
+	{ path: "**", title: "Oops. 404", component: PageNotFoundComponent },
+];
---
+++ src/app/published/published.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-published',
+  imports: [],
+  templateUrl: './published.component.html',
+  styleUrl: './published.component.scss'
+})
+export class PublishedComponent {
+
+}
