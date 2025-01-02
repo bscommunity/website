f2cd7ffa589048c28b990ff97a1289104cf59489
Eduardo Maciel <eduardomacielbr@gmail.com>
2025-01-02 00:05:18 -0300

    🧱 feat: wip page and features template

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,10 @@ import type { Routes } from "@angular/router";
 // Routes
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
 import { PublishedComponent } from "./published/published.component";
+import { OverviewComponent } from "./overview/overview.component";
 
 export const routes: Routes = [
+	{ path: "overview", component: OverviewComponent, title: "Overview" },
 	{ path: "published", component: PublishedComponent, title: "Published" },
 	{ path: "", redirectTo: "/published", pathMatch: "full" },
 	// The 404 page should be the last
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,13 +1,37 @@
-import { ChangeDetectionStrategy, Component } from "@angular/core";
+import { ChangeDetectionStrategy, Component, inject } from "@angular/core";
+
+import { RouterLink, RouterLinkActive } from "@angular/router";
 
 import { MatIconModule } from "@angular/material/icon";
 import { MatButtonModule } from "@angular/material/button";
 import { MatMenuModule } from "@angular/material/menu";
+import { MatSnackBar } from "@angular/material/snack-bar";
+import { MatRippleModule } from "@angular/material/core";
 
 @Component({
 	selector: "app-header",
 	templateUrl: "./header.component.html",
-	imports: [MatIconModule, MatMenuModule, MatButtonModule],
+	imports: [
+		MatIconModule,
+		MatMenuModule,
+		MatButtonModule,
+		MatRippleModule,
+		RouterLink,
+		RouterLinkActive,
+	],
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+	private _snackBar = inject(MatSnackBar);
+
+	openWarning() {
+		this._snackBar.open(
+			"This feature has not been implemented yet!",
+			"Ok",
+			{
+				horizontalPosition: "right",
+				verticalPosition: "bottom",
+			},
+		);
+	}
+}
