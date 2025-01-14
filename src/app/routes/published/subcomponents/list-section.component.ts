0557416e0012337e524e07371233863be710b97f
meninocoiso <225307799+meninocoiso@users.noreply.github.com>
2025-01-14 13:01:08 -0300

    🎨 fix: published page mobile support

--- src/app/routes/published/subcomponents/list-section.component.ts
+++ src/app/routes/published/subcomponents/list-section.component.ts
@@ -6,7 +6,7 @@ import { Component, Input } from "@angular/core";
 	template: `
 		<div class="flex flex-col items-start justify-start gap-4 w-full">
 			<h5 class="mat-headline-small text-2xl">{{ title }}</h5>
-			<ul class="grid grid-cols-2 w-full gap-6">
+			<ul class="flex flex-col md:grid md:grid-cols-2 w-full gap-6">
 				<ng-content></ng-content>
 			</ul>
 		</div>
