76a8f2bedeed431602a120bfa5ba4aecf53ef056
Eduardo Maciel <eduardomacielbr@gmail.com>
2025-01-21 11:18:59 -0300

    ✨ feat: initial upload dialog

--- src/app/components/header/subcomponents/section-button.component.ts
+++ src/app/components/header/subcomponents/section-button.component.ts
@@ -4,19 +4,19 @@ import { Component, Input, Output, EventEmitter } from "@angular/core";
 @Component({
 	selector: "app-section-button",
 	template: `
-    <button
-      *ngIf="isActive; else outlinedButton"
-      mat-flat-button
-      (click)="handleClick()"
-    >
-      <ng-content></ng-content>
-    </button>
-    <ng-template #outlinedButton>
-      <button mat-stroked-button (click)="handleClick()">
-        <ng-content></ng-content>
-      </button>
-    </ng-template>
-  `,
+		<button
+			*ngIf="isActive; else outlinedButton"
+			mat-flat-button
+			(click)="handleClick()"
+		>
+			<ng-content></ng-content>
+		</button>
+		<ng-template #outlinedButton>
+			<button mat-stroked-button (click)="handleClick()">
+				<ng-content></ng-content>
+			</button>
+		</ng-template>
+	`,
 	imports: [NgIf],
 })
 export class SectionButtonComponent {
