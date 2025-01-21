08caeaeb0a8b28bc2ac93de62b376ba6870cdc31
meninocoiso <225307799+meninocoiso@users.noreply.github.com>
2025-01-21 13:18:04 -0300

    ✨ feat: multi-step form

--- src/app/routes/chart/subcomponents/aside.component.ts
+++ src/app/routes/chart/subcomponents/aside.component.ts
@@ -6,14 +6,14 @@ import { AsideContainerComponent } from "./aside-container.component";
 	selector: "app-aside",
 	template: `
 		<aside class="flex flex-col items-start justify-start gap-6 w-full">
-			<div class="absolute top-0 left-0 w-full max-md:-z-10">
+			<div class="absolute top-0 left-0 md:relative w-full max-md:-z-10">
 				<img
-					class="w-full opacity-50 md:opacity-10 md:w-72 md:h-72 md:min-w-72 aspect-square md:rounded-lg"
+					class="w-full opacity-50 md:opacity-100 md:w-72 md:h-72 md:min-w-72 aspect-square md:rounded-lg"
 					src="https://i0.wp.com/lyricsfa.com/wp-content/uploads/2018/10/The-Prodigy-Lyrics.jpg?fit=1000%2C1000&ssl=1"
 					alt="The Prodigy"
 				/>
 				<div
-					class="absolute bottom-0 left-0 h-24 w-full bg-gradient-to-b from-transparent to-background"
+					class="flex md:hidden absolute bottom-0 left-0 h-24 w-full bg-gradient-to-b from-transparent to-background"
 				></div>
 			</div>
 			<app-aside-section title="Chart Info" class="w-full">
