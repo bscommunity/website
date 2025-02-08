c24e6fd94be96a2966959cdee8bfca9623e3fb4c
meninocoiso <225307799+meninocoiso@users.noreply.github.com>
2025-02-08 02:34:08 -0300

    🌀 fix: switched name from ContributorUser to SimplifiedUser

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -12,11 +12,11 @@ export const User = z.object({
 
 export type UserModel = z.infer<typeof User>;
 
-export const ContributorUser = User.omit({
+export const SimplifiedUser = User.omit({
 	email: true,
 	discordId: true,
 	id: true,
 	createdAt: true,
 });
 
-export type ContributorUserModel = z.infer<typeof ContributorUser>;
+export type SimplifiedUserModel = z.infer<typeof SimplifiedUser>;
