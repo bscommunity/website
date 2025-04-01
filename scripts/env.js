a88f7cc5f63854eda836884a370a880c956d5f17
meninocoiso <225307799+meninocoiso@users.noreply.github.com>
2025-04-01 00:39:25 -0300

    🐞 fix: environment variables now should be working when hosting

---
+++ scripts/env.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const path = require('path');
+const successColor = '\x1b[32m%s\x1b[0m';
+const checkSign = '\u{2705}';
+require('dotenv').config({ path: '.env' });;
+
+const envFile = `export const environment = {
+	DISCORD_CLIENT_ID: '${process.env.DISCORD_CLIENT_ID}',
+	REDIRECT_URI: '${process.env.REDIRECT_URI}',
+	LASTFM_API_KEY: '${process.env.LASTFM_API_KEY}',
+	SPOTIFY_CLIENT_ID: '${process.env.SPOTIFY_CLIENT_ID}',
+	SPOTIFY_API_KEY: '${process.env.SPOTIFY_API_KEY}',
+	ENCODING_KEY: '${process.env.ENCODING_KEY}',
+};
+`;
+const targetPath = path.join(__dirname, '../src/environments/environment.ts');
+fs.writeFile(targetPath, envFile, (err) => {
+    if (err) {
+        console.error(err);
+        throw err;
+    } else {
+        console.log(successColor, `${checkSign} Successfully generated environment.ts`);
+    }
+});
