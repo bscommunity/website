4a9ba696e5288de62e76c5812b25dcbda5a986d7
meninocoiso <225307799+meninocoiso@users.noreply.github.com>
2025-04-02 11:03:38 -0300

    🐞 fix: added production mode enabling to builds

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
 import { bootstrapApplication } from "@angular/platform-browser";
+import { enableProdMode } from "@angular/core";
 import { appConfig } from "./app/app.config";
 import { AppComponent } from "./app/app.component";
+import { environment } from "environments/environment";
+
+if (environment.PRODUCTION !== "false") {
+	// If the environment is production, enable production mode
+	enableProdMode();
+}
 
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
 	console.error(err),
