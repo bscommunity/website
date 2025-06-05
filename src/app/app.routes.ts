aadf9640fc38a25ba56e279526b30461a39a429c
Eduardo Maciel <eduardomacielbr@gmail.com>
2025-06-05 11:33:44 -0300

    🔧refactor: migrated to lazy loading routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,25 +1,25 @@
 import type { Routes } from "@angular/router";
 
-import { LandingComponent } from "./routes/landing.component";
+
 
 // Routes
-import { PublishedComponent } from "./routes/published/published.component";
-import { OverviewComponent } from "./routes/overview/overview.component";
-import { ChartComponent } from "./routes/chart/chart.component";
+
+
+
 
 // Linking Routes
-import { LinkComponent } from "./routes/link/link.component";
-import { PrivacyPolicyComponent } from "./routes/privacy-policy/privacy-policy.component";
-import { TermsOfServiceComponent } from "./routes/terms-of-service/terms-of-service.component";
-import { ReleaseNotesComponent } from "./routes/release-notes/release-notes.component";
+
+
+
+
 
 // Error Routes
-import { PageErrorComponent } from "./routes/error/error.component";
-import { PageNotFoundComponent } from "./routes/not-found/not-found.component";
+
+
 
 // Auth Routes
-import { LoginComponent } from "./routes/auth/login/login.component";
-import { OAuthCallbackComponent } from "./routes/auth/callback/callback.component";
+
+
 
 // Resolvers
 import { ChartResolver } from "./routes/chart/chart-resolver.service";
@@ -31,64 +31,64 @@ import {
 export const routes: Routes = [
 	{
 		path: "login",
-		component: LoginComponent,
+		loadComponent: () => import('./routes/auth/login/login.component').then(m => m.LoginComponent),
 		canActivate: [redirectIfAuthenticatedGuard],
 		title: "Login",
 	},
 	{
 		path: "callback",
-		component: OAuthCallbackComponent,
+		loadComponent: () => import('./routes/auth/callback/callback.component').then(m => m.OAuthCallbackComponent),
 		canActivate: [redirectIfAuthenticatedGuard],
 		title: "Authenticating...",
 	},
 	{
 		path: "overview",
-		component: OverviewComponent,
+		loadComponent: () => import('./routes/overview/overview.component').then(m => m.OverviewComponent),
 		canActivate: [isAuthenticatedGuard],
 		title: "Overview",
 	},
 	{
 		path: "published",
-		component: PublishedComponent,
+		loadComponent: () => import('./routes/published/published.component').then(m => m.PublishedComponent),
 		canActivate: [isAuthenticatedGuard],
 		title: "Published",
 	},
 	{
 		path: "chart/:id",
-		component: ChartComponent,
+		loadComponent: () => import('./routes/chart/chart.component').then(m => m.ChartComponent),
 		resolve: { chart: ChartResolver },
 		canActivate: [isAuthenticatedGuard],
 		runGuardsAndResolvers: "always",
 	},
 	{
 		path: "release-notes",
-		component: ReleaseNotesComponent,
+		loadComponent: () => import('./routes/release-notes/release-notes.component').then(m => m.ReleaseNotesComponent),
 		title: "Release Notes",
 	},
 	{
 		path: "privacy-policy",
-		component: PrivacyPolicyComponent,
+		loadComponent: () => import('./routes/privacy-policy/privacy-policy.component').then(m => m.PrivacyPolicyComponent),
 		title: "Our Privacy Policy: Transparency for Our Community",
 	},
 	{
 		path: "terms-of-service",
-		component: TermsOfServiceComponent,
+		loadComponent: () => import('./routes/terms-of-service/terms-of-service.component').then(m => m.TermsOfServiceComponent),
 		title: "Our Terms of Service",
 	},
 	{
 		path: "link/:type/:id",
-		component: LinkComponent,
+		loadComponent: () => import('./routes/link/link.component').then(m => m.LinkComponent),
 	},
 	{
 		path: "error",
-		component: PageErrorComponent,
+		loadComponent: () => import('./routes/error/error.component').then(m => m.PageErrorComponent),
 		title: "Error",
 	},
 	{
 		path: "",
-		component: LandingComponent,
+		loadComponent: () => import('./routes/landing.component').then(m => m.LandingComponent),
 		title: "bscm",
 	},
 	// { path: "", redirectTo: "/published", pathMatch: "full" }, // Redirect to published
-	{ path: "**", title: "Oops. 404", component: PageNotFoundComponent }, // Should be the last
+	{ path: "**", title: "Oops. 404", loadComponent: () => import('./routes/not-found/not-found.component').then(m => m.PageNotFoundComponent) }, // Should be the last
 ];
