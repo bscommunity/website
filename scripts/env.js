b52c4b058c1a7598312765b4c49667eabc8d54de
meninocoiso <225307799+meninocoiso@users.noreply.github.com>
2025-08-14 20:10:22 -0300

    fix: revert all changes to env.js

--- scripts/env.js
+++ scripts/env.js
@@ -2,22 +2,23 @@ const fs = require("fs");
 const path = require("path");
 const successColor = "\x1b[32m%s\x1b[0m";
 const checkSign = "\u{2705}";
-
 require("dotenv").config({ path: ".env" });
 
-const envVars = Object.entries(process.env)
-	.map(([key, value]) => {
-		if (value === 'true' || value === 'false') {
-			return `  ${key}: ${value},`;
-		} else if (!isNaN(value) && value !== '' && value !== null) {
-			return `  ${key}: ${value},`;
-		} else {
-			return `  ${key}: '${value}',`;
-		}
-	})
-	.join('\n');
-
-const envFile = `export const environment = {\n${envVars}\n};\n`;
+const envFile = `export const environment = {
+    PRODUCTION: ${process.env.PRODUCTION},
+    API_URL: '${process.env.API_URL}',
+	DISCORD_CLIENT_ID: '${process.env.DISCORD_CLIENT_ID}',
+	REDIRECT_URI: '${process.env.REDIRECT_URI}',
+	LASTFM_API_KEY: '${process.env.LASTFM_API_KEY}',
+	SPOTIFY_CLIENT_ID: '${process.env.SPOTIFY_CLIENT_ID}',
+	SPOTIFY_API_KEY: '${process.env.SPOTIFY_API_KEY}',
+	ENCODING_KEY: '${process.env.ENCODING_KEY}',
+	GOOGLE_CLIENT_ID: '${process.env.GOOGLE_CLIENT_ID}',
+	GOOGLE_CLIENT_SECRET: '${process.env.GOOGLE_CLIENT_SECRET}',
+	GOOGLE_REDIRECT_URI: '${process.env.GOOGLE_REDIRECT_URI}',
+	GOOGLE_SCOPES: '${process.env.GOOGLE_SCOPES}',
+};
+`;
 const targetPath = path.join(process.cwd(), "src/environments/environment.ts");
 
 // Create the target directory if it doesn't exist
